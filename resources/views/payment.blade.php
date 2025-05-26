ee74fd9d8ad50cc436ddf58ea11076432ea2d9fb
Nada Nasr <nadanm432@gmail.com>
2025-05-26 11:35:37 +0300

    remove comments from blade

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -64,10 +64,8 @@
         </div>
     </div>
 
-    <!-- Bootstrap JS (Optional) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     
-    <!-- AJAX Logic -->
     <script>
         document.getElementById('payment-form').addEventListener('submit', async function (e) {
             e.preventDefault();
